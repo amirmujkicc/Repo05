b487767ead9a10ddb398e9c7565cabfe865191f7
Amir Mujkic <mujkicc@gmail.com>
2021-09-17 15:53:46 +0200

    More concatenting strings

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var myString = "Concatentation is the process of appending ";
+    myString += "one string to the end of another string.";
+    // Only change code above this line
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
